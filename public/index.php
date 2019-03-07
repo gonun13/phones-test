98c69a711acdc25f02b762b1b44e2535a18b63e9
Nuno Gomes <gonun13@gmail.com>
2019-03-07 23:55:58 +0000

    algorithm test

--- public/index.php
+++ public/index.php
@@ -18,27 +18,57 @@ if (isset($_REQUEST['paginate']) && $_REQUEST['paginate'])
     $paginate = $_REQUEST['paginate'];
 }
 
+// build regex map
+$regexMap = array(
+    array('Cameroon', '+237', '\(237\)\ ?[2368]\d{7,8}$'),
+    array('Ethiopia','+251', '\(251\)\ ?[1-59]\d{8}$'),
+    array('Morocco', '+212', '\(212\)\ ?[5-9]\d{8}$'),
+    array('Mozambique', '+258', '\(258\)\ ?[28]\d{7,8}$'),
+    array('Uganda', '+256', '\(256\)\ ?\d{9}$'),
+);
+
+// read db
 $dbfile = $_SERVER['DOCUMENT_ROOT'] . '/../storage/sample.db';
-var_dump($dbfile);
+
 // open db
 $db = new SQLite3($dbfile, SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 $tablesquery = $db->query("SELECT * FROM customer");
 
-    while ($table = $tablesquery->fetchArray(SQLITE3_ASSOC)) {
-        var_dump( $table) . '<br />';
+// build results
+$results = array();
+while ($table = $tablesquery->fetchArray(SQLITE3_ASSOC)) {
+    $parts = explode(' ', $table['phone']);
+    $country = $country_code = $valid = false;
+    foreach ($regexMap as $regex)
+    {
+        // skip unmatched filter
+        if (isset($filterCountry) && $filterCountry != $regex[0]) continue;        
+        // check for country code first
+        if (preg_match("/\(".preg_replace("/\+/", "", $regex[1])."\)/", $parts[0]))
+        {
+            $country = $regex[0];
+            $country_code = $regex[1];
+        }
+        // check for valid number
+        if (preg_match("/$regex[2]/", $table['phone']))
+        {
+            $results['valid'][] = array(
+                'country' => $country,
+                'state' => 'OK',
+                'country_code' => $country_code,
+                'phone_number' => $parts[1],
+            );
+            $valid = true;
+        }
     }
-
-/** 
-// read db
-if ($db = sqlite_open('mysqlitedb', 0666, $sqliteerror)) { 
-    sqlite_query($db, 'CREATE TABLE foo (bar varchar(10))');
-    sqlite_query($db, "INSERT INTO foo VALUES ('fnord')");
-    $result = sqlite_query($db, 'select bar from foo');
-    var_dump(sqlite_fetch_array($result)); 
-} else {
-    die($sqliteerror);
+    if (!$valid && isset($country) && $country && isset($country_code) && $country_code)
+    {
+        $results['invalid'][] = array(
+            'country' => $country,
+            'state' => 'NOK',
+            'country_code' => $country_code,
+            'phone_number' => $parts[1],
+        );
+    }
 }
-*/
-
-// response list
-phpinfo();
+var_dump($results);
